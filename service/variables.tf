0b49781db8b657569eac386dedf0ee43b704d13e
jperez3 <joe@ordisius.com>
2022-02-24 09:02:17 -0600

    cleaning up service folder

--- service/variables.tf
+++ service/variables.tf
@@ -1,18 +1,3 @@
 variable "service" {
   description = "unique service name"
 }
-
-variable "enable_github_repo" {
-  description = "enables/disables creation of github repo"
-  default     = 1
-}
-
-variable "enable_ecr" {
-  description = "enables/disables creation of ecr repo"
-  default     = 1
-}
-
-variable "enable_dd_monitor" {
-  description = "enables/disables creation of datadog monitor"
-  default     = 1
-}
